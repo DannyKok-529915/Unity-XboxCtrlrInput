6857df9378af00d1c18c0b67593211d9f1123173
Chenoa van Wingerden <529944@student.glu.nl>
2020-02-18 12:16:59 +0100

    pmkjh

---
+++ XboxCtrlrInput/Assets/Dev Folders/Scripts/ButtonManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using XboxCtrlrInput;
+
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject startScherm;
+    public GameObject controlsScherm;
+    public GameObject creditsScherm;
+
+    [SerializeField] private Button startButton;
+
+    private XboxController controller;
+
+    private void Update()
+    {
+        if (XCI.GetButtonDown(XboxButton.B, controller) && startScherm.activeSelf == false)
+        {
+            StartScherm();
+            
+            startButton.Select();
+
+        }
+    }
+
+
+    public void StartScherm()
+    {
+        startScherm.SetActive(true);
+        controlsScherm.SetActive(false);
+        creditsScherm.SetActive(false);
+
+    }
+   
+
+    public void ControlsScherm()
+    {
+        startScherm.SetActive(false);
+        controlsScherm.SetActive(true);
+        creditsScherm.SetActive(false);
+
+    }
+
+    public void CreditsScherm()
+    {
+        startScherm.SetActive(false);
+        controlsScherm.SetActive(false);
+        creditsScherm.SetActive(true);
+
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
