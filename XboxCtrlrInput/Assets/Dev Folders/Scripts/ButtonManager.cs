a12837e6291250007cdf11ab78cd27984e632f2d
Chenoa van Wingerden <529944@student.glu.nl>
2020-02-18 09:10:40 +0100

    startscherm buttons werken met muis

---
+++ XboxCtrlrInput/Assets/Dev Folders/Scripts/ButtonManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject startScherm;
+    public GameObject controlsScherm;
+    public GameObject creditsScherm;
+    public GameObject goBackButtonScherm;
+
+    public void StartScherm()
+    {
+        startScherm.SetActive(true);
+        controlsScherm.SetActive(false);
+        creditsScherm.SetActive(false);
+
+        goBackButtonScherm.SetActive(false);
+    }
+
+    public void ControlsScherm()
+    {
+        startScherm.SetActive(false);
+        controlsScherm.SetActive(true);
+        creditsScherm.SetActive(false);
+
+        goBackButtonScherm.SetActive(true);
+    }
+
+    public void CreditsScherm()
+    {
+        startScherm.SetActive(false);
+        controlsScherm.SetActive(false);
+        creditsScherm.SetActive(true);
+
+        goBackButtonScherm.SetActive(true);
+    }
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
